9a96933707adce3cb149f9ae630fc7088292eeff
Kirill Troitsky <V-Troitsky@yandex.ru>
2020-06-24 20:42:05 +0300

    added route to invalid URL

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const path = require('path');
 const usersArr = require('./routes/users');
 const cardsArr = require('./routes/cards');
 const usersIdArr = require('./routes/usersid');
+const invalidRout = require('./routes/invalid');
 
 const { PORT = 3000 } = process.env;
 
@@ -12,5 +13,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', usersArr);
 app.use('/', cardsArr);
 app.use('/', usersIdArr);
+app.use('/', invalidRout);
 
 app.listen(PORT);
--- routes/invalid.js
+++ routes/invalid.js
@@ -1,7 +1,5 @@
-const invalidRouter = require("express").Router();
+const invalidRouter = require('express').Router();
 
-invalidRouter.get("*", (req, res) => {
-  return res.status(404).send({ "message": "Запрашиваемый ресурс не найден" });
-});
+invalidRouter.get('*', (req, res) => res.status(404).send({ message: 'Запрашиваемый ресурс не найден' }));
 
 module.exports = invalidRouter;
