08b06a23a719c61fcb8f65d15b1dcbe96dba7f2c
Kirill Troitsky <V-Troitsky@yandex.ru>
2020-06-25 20:34:10 +0300

    update npm run start and router.all for invalid URL

--- routes/invalid.js
+++ routes/invalid.js
@@ -1,5 +1,5 @@
 const invalidRouter = require('express').Router();
 
-invalidRouter.get('*', (req, res) => res.status(404).send({ message: 'Запрашиваемый ресурс не найден' }));
+invalidRouter.all('*', (req, res) => res.status(404).send({ message: 'Запрашиваемый ресурс не найден' }));
 
 module.exports = invalidRouter;
